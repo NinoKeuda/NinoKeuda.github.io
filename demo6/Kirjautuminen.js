b5fe36928bb6fa2478dc09ef9627f293bf3573ce
NinoKeuda <nino.mantere@edu.keuda.fi>
2023-03-23 13:22:18 +0200

    demo6 kirjautuminen

---
+++ demo6/Kirjautuminen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+    let kirjautunut = 'kylla';
+    if(kirjautunut === 'kylla'){
+        document.getElementByld('tervetulo_teksti').textContent += 'Nino!';
+}
+}
