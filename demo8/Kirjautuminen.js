bf43cf5e666070c9cdd19d7b9999fb7628e85f59
NinoKeuda <nino.mantere@edu.keuda.fi>
2023-05-08 14:15:26 +0300

    korjattu ulos nappi

--- demo8/Kirjautuminen.js
+++ demo8/Kirjautuminen.js
@@ -1,13 +1,36 @@
-document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+document.addEventListener("DOMContentLoaded", tarkistaKirjautuminen);
 
-function onkoKirjautunut() {
-   if (localStorage.getItem("kirjautunut") === "kylla") {
-        document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
-}
+function tarkistaKirjautuminen() {
+  // Tarkistetaan, onko käyttäjä kirjautunut sisään
+  if (localStorage.getItem("kirjautunut") === "kylla") {
+    // Jos käyttäjä on kirjautunut sisään, lisätään hänen nimensä tervehdystekstiin
+    const tervetuloTeksti = document.getElementById("tervetulo_teksti");
+    tervetuloTeksti.textContent += localStorage.getItem("nimi");
+  }
 }
 
 function kirjaudu() {
-localStorage.setItem("nimi", document.getElementById("nimi").value);
-localStorage.setItem("kirjaudu_ulos", document.getElementById("kirjaudu_ulos").value);
-localStorage.setItem("kirjautunut", "kylla");
+  // Tallennetaan käyttäjän syöttämä nimi Local Storageen
+  const nimi = document.getElementById("nimi").value;
+  localStorage.setItem("nimi", nimi);
+
+  // Tallennetaan "Kirjaudu ulos" -napin elementti Local Storageen
+  const kirjauduUlos = document.getElementById("kirjaudu_ulos");
+  localStorage.setItem("kirjaudu_ulos", kirjauduUlos);
+
+  // Merkitään käyttäjä kirjautuneeksi sisään
+  localStorage.setItem("kirjautunut", "kylla");
+
+  // Piilotetaan kirjautumislomake ja näytetään tervehdysteksti
+  document.getElementById("kirjautumislomake").style.display = "none";
+  document.getElementById("tervetulo_teksti").style.display = "block";
+}
+
+function kirjauduUlos() {
+  // Poistetaan kaikki Local Storagen tallennetut tiedot
+  localStorage.clear();
+
+  // Näytetään kirjautumislomake ja piilotetaan tervehdysteksti
+  document.getElementById("kirjautumislomake").style.display = "block";
+  document.getElementById("tervetulo_teksti").style.display = "none";
 }
